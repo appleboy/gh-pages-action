77e5743237f4e772bb16c3237ce109ae7a2d101e
Bo-Yi Wu <appleboy.tw@gmail.com>
2020-03-08 22:50:31 +0800

    chore: add default author information.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,7 @@
 
 set -eu
 
-sh -c "/bin/drone-gh-pages $*"
+INPUT_COMMIT_AUTHOR_EMAIL=${INPUT_COMMIT_AUTHOR_EMAIL:-'github-action@users.noreply.github.com'}
+INPUT_COMMIT_AUTHOR=${INPUT_COMMIT_AUTHOR:-'GitHub Action'}
+
+sh -c "/bin/drone-gh-pages"
